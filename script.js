821666c4c16ccf128216c58ae6746f172d4851a2
Akash <therealakash13@gmail.com>
2025-07-04 07:33:23 +0000

    portfolio section

--- script.js
+++ script.js
@@ -15,3 +15,48 @@ resumeBtns.forEach((btn, idx) => {
     resumeDetails[idx].classList.add("active");
   });
 });
+
+const arrowRight = document.querySelector(
+  ".portfolio-box .navigation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .navigation .arrow-left"
+);
+
+let index = 0;
+
+const activePortfolio = () => {
+  const imgSlide = document.querySelector(".portfolio-carousel .img-slide");
+  const portfolioDetails = document.querySelectorAll(".portfolio-detail");
+
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
+  portfolioDetails.forEach((detail) => {
+    detail.classList.remove("active");
+  });
+  portfolioDetails[index].classList.add("active");
+};
+
+arrowRight.addEventListener("click", () => {
+  if (index < 4) {
+    index++;
+    arrowLeft.classList.remove("disabled");
+  } else {
+    index = 5;
+    arrowRight.classList.add("disabled");
+  }
+
+  activePortfolio();
+});
+arrowLeft.addEventListener("click", () => {
+  if (index > 1) {
+    index--;
+    arrowRight.classList.remove("disabled");
+  } else {
+    index = 0;
+    arrowLeft.classList.add("disabled");
+  }
+
+  activePortfolio();
+});
